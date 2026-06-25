387df35af97b0695bcdd3a94eec0f2538eca25cf
Pierrre <pierredanjou@MacBook-Air-de-pierre.local>
2026-06-25 13:43:01 +0200

    Sécurisation clés Supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,2 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabase = createClient("https://twaisundpzhbztvrlglg.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3YWlzdW5kcHpoYnp0dnJsZ2xnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNDY5NjQsImV4cCI6MjA5NjgyMjk2NH0.ylPetbE9gI8g_5-dlaOUEI0msssUqfEwyyQh8PK6A8Q");
+
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
